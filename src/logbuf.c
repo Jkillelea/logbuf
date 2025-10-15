2fe8b37acd118d4dfea5f44fcf4286eec354c57b
Jacob Killelea <jkillelea344@gmail.com>
2025-10-15 12:01:02 -0700

    Bugfix. Flush from ring buffer

--- src/logbuf.c
+++ src/logbuf.c
@@ -1,3 +1,4 @@
+#include "ringbuffer.h"
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -14,33 +15,42 @@ typedef enum
 
 static log_level_t current_level = ERROR;
 static char logbuffer[LOG_MAX] = {};
-static size_t idx = 0;
+static struct ringbuffer buf;
 
 static void log(log_level_t level, const char *const fmt, ...)
 {
-    size_t newindex = 0;
+    if (!buf.buffer)
+    {
+        buf = ringbuffer_alloc(4096);
+    }
+
+    size_t len = 0;
     va_list list;
     va_start(list, fmt);
 
     // TODO: we should use a ring buffer here. Keep the size static and just
     // overwrite. To do that we will probably need a separate buffer for each log
     // message and then we will append them to a ring buffer.
-    newindex = (size_t)snprintf(&logbuffer[idx], LOG_MAX, fmt, list);
+    len = (size_t)snprintf(logbuffer, LOG_MAX, fmt, list);
 
     va_end(list);
 
+    // Don't append null terminator
+    ringbuffer_append(&buf, logbuffer, len);
+
     if (level >= current_level)
     {
-        printf("%s", &logbuffer[idx]);
+        printf("%s", logbuffer);
     }
-
-    idx = newindex % LOG_MAX;
 }
 
 static void flush_buffer(void)
 {
-    printf("%s", logbuffer);
-    memset(logbuffer, 0, sizeof(logbuffer));
+    while (ringbuffer_remove(&buf, logbuffer, sizeof(logbuffer)) > 0)
+    {
+        printf("%s\n", logbuffer);
+        memset(logbuffer, 0, sizeof(logbuffer));
+    }
 }
 
 // Error logs trigger a flush of the logging buffer
